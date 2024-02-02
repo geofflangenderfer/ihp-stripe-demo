596f2471e066aac30567e262ceee2cd4cd2abec4
Geoff Langenderfer <geoff.langenderfer@gmail.com>
2024-02-02 10:06:31 -0800

    feat: add db columns

--- Application/Schema.sql
+++ Application/Schema.sql
@@ -10,7 +10,10 @@ CREATE TABLE users (
     stripe_customer_id TEXT NOT NULL,
     created_at TIMESTAMP WITH TIME ZONE DEFAULT NOW() NOT NULL,
     updated_at TIMESTAMP WITH TIME ZONE DEFAULT NOW() NOT NULL,
-    subscription_id UUID NOT NULL
+    subscription_id UUID NOT NULL,
+    first_name TEXT NOT NULL,
+    last_name TEXT NOT NULL,
+    email TEXT NOT NULL
 );
 CREATE TRIGGER update_users_updated_at BEFORE UPDATE ON users FOR EACH ROW EXECUTE FUNCTION set_updated_at_to_now();
 CREATE TABLE subscriptions (
@@ -26,4 +29,3 @@ CREATE TRIGGER update_subscriptions_updated_at BEFORE UPDATE ON subscriptions FO
 CREATE INDEX subscriptions_created_at_index ON subscriptions (created_at);
 ALTER TABLE subscriptions ADD CONSTRAINT subscriptions_ref_user_id FOREIGN KEY (user_id) REFERENCES users (id) ON DELETE NO ACTION;
 ALTER TABLE users ADD CONSTRAINT users_ref_subscription_id FOREIGN KEY (subscription_id) REFERENCES subscriptions (id) ON DELETE NO ACTION;
-
