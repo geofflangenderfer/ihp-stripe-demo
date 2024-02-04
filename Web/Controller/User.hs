e1a922ef591c84f49767ddfbec2596858b895568
Geoff Langenderfer <geoff.langenderfer@gmail.com>
2024-02-04 10:51:20 -0800

    feat: successfully create stripe customer
    this change successfully calls stripe and creates a new stripe customer.
    
    It's not currently saving a user to the users table:
    
    null value in column "subscription_id" of relation "users" violates not-null constraint
    While running the following Query:
    "INSERT INTO users (id, stripe_customer_id, created_at, updated_at, subscription_id, first_name, last_name, email) VALUES (?, ?, ?, ?, ?, ?, ?, ?) RETURNING id, stripe_customer_id, created_at, updated_at, subscription_id, first_name, last_name, email"
    With Query Parameters:
    [Plain "DEFAULT",Escape "cus_PVB1whpZU0IgUG",Plain "DEFAULT",Plain "DEFAULT",Plain "DEFAULT",Escape "geoff",Escape "langenderfer",Escape "geoff.langenderfer@gmail.com"]
    Details:
    The exception was raised while running the action: CreateUserAction
    
    EnhancedSqlError {sqlErrorQuery = "INSERT INTO users (id, stripe_customer_id, created_at, updated_at, subscription_id, first_name, last_name, email) VALUES (?, ?, ?, ?, ?, ?, ?, ?) RETURNING id, stripe_customer_id, created_at, updated_at, subscription_id, first_name, last_name, email", sqlErrorQueryParams = [Plain "DEFAULT",Escape "cus_PVB1whpZU0IgUG",Plain "DEFAULT",Plain "DEFAULT",Plain "DEFAULT",Escape "geoff",Escape "langenderfer",Escape "geoff.langenderfer@gmail.com"], sqlError = SqlError {sqlState = "23502", sqlExecStatus = FatalError, sqlErrorMsg = "null value in column \"subscription_id\" of relation \"users\" violates not-null constraint", sqlErrorDetail = "Failing row contains (c18f234d-7f88-45ed-8026-927d77a0dd87, cus_PVB1whpZU0IgUG, 2024-02-04 10:43:57.242563-08, 2024-02-04 10:43:57.242563-08, geoff, langenderfer, geoff.langenderfer@gmail.com, null).", sqlErrorHint = ""}}
    
    Ask the IHP Community on StackOverflowCheck the TroubleshootingOpen GitHub IssueSlackRedditStackShare

--- Web/Controller/User.hs
+++ Web/Controller/User.hs
@@ -1,10 +1,21 @@
 module Web.Controller.User where
 
+-- import StripeAPI.Common (runWithConfiguration)
+-- import StripeAPI.Configuration (defaultConfiguration)
+-- import StripeAPI.Operations.PostCustomers (postCustomers, PostCustomersResponse200, mkPostCustomersRequestBody)
+-- import StripeAPI.SecuritySchemes (basicAuthenticationSecurityScheme, BasicAuthenticationData, basicAuthenticationDataUsername, basicAuthenticationDataPassword)
+-- import StripeAPI.Types.Customer.Customer
 import Web.Controller.Prelude
 import Web.View.User.Index
 import Web.View.User.New
 import Web.View.User.Edit
 import Web.View.User.Show
+import qualified Data.Text as T
+import qualified Network.HTTP.Simple as HS
+import qualified StripeAPI as Stripe
+import qualified StripeAPI.Types.NotificationEventData.Extra as Stripe
+import Control.Monad.Trans.Except (ExceptT, throwE, runExceptT)
+import IHP.ControllerPrelude
 
 instance Controller UserController where
     action UsersAction = do
@@ -36,14 +47,20 @@ instance Controller UserController where
 
     action CreateUserAction = do
         let user = newRecord @User
-        user
-            |> buildUser
-            |> ifValid \case
-                Left user -> render NewView { .. } 
-                Right user -> do
-                    user <- user |> createRecord
-                    setSuccessMessage "User created"
-                    redirectTo UsersAction
+        result <- createStripeCustomer
+        case result of
+            Nothing -> render NewView { .. }
+            Just customerId -> do
+                let userWithCustomerId = user |> set #stripeCustomerId customerId
+
+                userWithCustomerId
+                    |> buildUser
+                    |> ifValid \case
+                        Left user -> render NewView { .. }
+                        Right user -> do
+                            user <- user |> createRecord
+                            setSuccessMessage "User created"
+                            redirectTo UsersAction
 
     action DeleteUserAction { userId } = do
         user <- fetch userId
@@ -52,4 +69,36 @@ instance Controller UserController where
         redirectTo UsersAction
 
 buildUser user = user
-    |> fill @'["stripeCustomerId", "subscriptionId"]
+    |> fill @'["firstName", "lastName", "email"]
+
+--createStripeCustomer :: IO (Either Text Text)
+createStripeCustomer :: (?context::ControllerContext) => IO (Maybe Text)
+createStripeCustomer = do
+    let body = Stripe.mkPostCustomersRequestBody { Stripe.postCustomersRequestBodyName = Just $ (param "firstName") ++ " " ++ (param "lastName")
+        , Stripe.postCustomersRequestBodyEmail = Just (param "email")
+        }
+
+    let config = Stripe.basicAuthenticationSecurityScheme Stripe.BasicAuthenticationData { basicAuthenticationDataUsername = "sk_test_51MPCmeIa2YCAcFeYz8ij4I14wzfDqJv8UsMAflU79yP9BA89vQENF5A76D30UQ6eznP3mgXg1PanDB5Md9LfXcBP00EsSThdQq",
+            basicAuthenticationDataPassword = ""
+          }
+
+    let conf = Stripe.defaultConfiguration { Stripe.configSecurityScheme = config}
+
+    --resp <- Stripe.runWithConfiguration conf $ Stripe.postCustomers (Just body)
+    resp <- liftIO $ Stripe.runWithConfiguration conf $ Stripe.postCustomers (Just body)
+
+    -- print resp
+    case HS.getResponseBody resp of
+        Stripe.PostCustomersResponse200 customer -> pure $ Just (Stripe.customerId customer)
+        -- PostCustomers.PostCustomersResponseError error -> Left
+        -- PostCustomers.PostCustomersResponseDefault error -> Left
+        _ -> pure Nothing
+
+{-
+
+curl https://api.stripe.com/v1/customers \
+  -u "sk_test_51MPCmeIa2YCAcFeYz8ij4I14wzfDqJv8UsMAflU79yP9BA89vQENF5A76D30UQ6eznP3mgXg1PanDB5Md9LfXcBP00EsSThdQq:" \
+  -d name="Jenny Rosen" \
+  --data-urlencode email="jennyrosen@example.com"
+
+-}
